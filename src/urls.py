907fc939b2409e32de64d67474222a8cd7c79861
he_yulin <he_yulin@163.com>
2018-12-29 10:26:28 +0800

    debug

--- src/urls.py
+++ src/urls.py
@@ -50,7 +50,7 @@ urlpatterns = [
     
     url(r'^d/',include('helpers.director.urls'),name='director'),
     
-    url(r'^$',RedirectView.as_view(url='/pc/inspector.inspector')),
+    url(r'^$',RedirectView.as_view(url='/cg')),
     
     url('^inspector_case/(?P<code>\w+)/?$',inspector_case)
 ]
